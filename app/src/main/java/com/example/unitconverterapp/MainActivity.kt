9aea84dfbf81a4a5ee4d7020ff2980a38e16b1f1
Master Luke Otieno <lukemclain4@gmail.com>
2024-09-29 22:49:16 +0300

    Box layouts
    Added buttons in the box layouts

--- app/src/main/java/com/example/unitconverterapp/MainActivity.kt
+++ app/src/main/java/com/example/unitconverterapp/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.example.unitconverterapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unitconverterapp.ui.theme.UnitConverterAppTheme
 
@@ -44,15 +51,28 @@ fun UnitConverter() {
         })
         Row {
             //Here our UI elements will be next to each other
+            Box {
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "Drop Down Arrow")
+                }
+            }
+            Box {
+Button(onClick = { /*TODO*/ }) {
+    Text(text = "Select")
+    Icon(Icons.Default.ArrowDropDown, contentDescription = "Drop Down Arrow")
+}
 
+            }
 
         }
+        Text(text = "Result:")
     }
 }
 
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-fun UnitConverterPreview(){
+fun UnitConverterPreview() {
     UnitConverter()
 }
