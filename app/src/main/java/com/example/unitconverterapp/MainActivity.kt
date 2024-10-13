bf254ba8f26452bb35b545c3817a620442822571
Master Luke Otieno <lukemclain4@gmail.com>
2024-10-13 23:27:23 +0300

    Adjusted the layout
    Added the verticalArrangement and horizontalAlignment layouts. Used a Spacer modifier to space the elements

--- app/src/main/java/com/example/unitconverterapp/MainActivity.kt
+++ app/src/main/java/com/example/unitconverterapp/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.unitconverterapp
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
@@ -17,9 +19,10 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverterapp.ui.theme.UnitConverterAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,13 +45,19 @@ class MainActivity : ComponentActivity() {
 //Let's add our own composable
 @Composable
 fun UnitConverter() {
-    //Since our func is a composable, we can use other Composables in it
-    Column {
+    //Since our func is a composable, we can use other composable in it
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         //Here our UI elements will be stacked on top of each other
         Text(text = "Unit Converter")
+        Spacer(modifier = Modifier.height(18.dp))
         OutlinedTextField(value = "", onValueChange = {
             //Here goes what should happen when the value of our OutlinedTextField changes
         })
+        Spacer(modifier = Modifier.height(18.dp))
         Row {
             //Here our UI elements will be next to each other
             Box {
